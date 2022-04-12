3210151f123ccf2d94c8ab51e9c7b307ef91da9b
ledhcg <dinhcuong.firewin99@gmail.com>
2022-04-13 02:22:24 +0300

    13 04 2022

--- build/app.js
+++ build/app.js
@@ -1,15 +1,1 @@
-var number = 1;
-let string;
-function sum(a, b) {
-    return a + b;
-}
-const multiply = (a, b) => a * b;
-function waitAndDo(callback) {
-    return setTimeout(() => callback('Hello'), 1000);
-}
-class Say {
-    sayHi(name) {
-        console.info('Hello, ', name);
-    }
-}
 //# sourceMappingURL=app.js.map
